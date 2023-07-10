9b7e0b325ad3da4b61492506cea40929209d8e78
mach4101 <mach4101@gmail.com>
2023-07-10 19:29:18 +0800

    pengpai page crawler

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mach4101/crawler
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	url := "https://www.thepaper.cn/"
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error status code %v", resp.StatusCode)
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("read content fail %v", err)
+		return
+	}
+
+	fmt.Println("body:", string(body))
+}
