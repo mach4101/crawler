821c08136dbbda6c670091ded0f22003555908f7
mach4101 <mach4101@gmail.com>
2023-07-12 17:23:10 +0800

    chromedp

--- go.mod
+++ go.mod
@@ -5,7 +5,16 @@ go 1.18
 require (
 	github.com/antchfx/htmlquery v1.3.0 // indirect
 	github.com/antchfx/xpath v1.2.3 // indirect
+	github.com/chromedp/cdproto v0.0.0-20230625224106-7fafe342e117 // indirect
+	github.com/chromedp/chromedp v0.9.1 // indirect
+	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.2.1 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
 	golang.org/x/text v0.11.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,20 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"time"
 
-	"github.com/mach4101/crawler/collect"
+	"github.com/chromedp/chromedp"
 )
 
 func main() {
-	url := "https://book.douban.com/subject/1007305"
+	ctx, cancel := chromedp.NewContext(
+		context.Background(),
+	)
+	defer cancel()
 
-	var f collect.Fetcher = collect.BrowserFetch{}
-	body, err := f.Get(url)
+	ctx, cancel = context.WithTimeout(ctx, 15*time.Second)
+	defer cancel()
+
+	// crawl pack
+	var example string
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(`https://pkg.go.dev/time`),
+		chromedp.WaitVisible(`body > footer`),
+		chromedp.Click(`#example-After`, chromedp.NodeVisible),
+		chromedp.Value(`#example-After textarea`, &example),
+	)
 	if err != nil {
-		fmt.Printf("there are exist error %v\n", err)
-		return
+		log.Fatal(err)
 	}
 
-	fmt.Println(string(body))
+	log.Printf("Go's time.After example:\\%s", example)
 }
